470ef6a723f12a1330f1daa7496122c8fcac0daa
sebrinamusbah <sebrinm9@gmail.com>
2026-06-23 14:00:52 +0300

    fix bcrypt password crash

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -5,8 +5,9 @@ pwd_context = CryptContext(
     deprecated="auto"
 )
 
+#  FIXED VERSION
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])  # <-- fix here
 
 def verify_password(password: str, hashed_password: str):
     return pwd_context.verify(password, hashed_password)
