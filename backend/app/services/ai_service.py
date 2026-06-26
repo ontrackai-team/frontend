52b1c744ff024381293f19827eab007da24e0bb1
sebrinamusbah <sebrinm9@gmail.com>
2026-06-26 15:27:22 +0300

    Update ai_service.py

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -2,8 +2,7 @@ import os
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def generate_text(prompt: str):
