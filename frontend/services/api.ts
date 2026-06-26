3b86d5fadcdec099f99e23892ecbd3fbadb0ae74
sebrinamusbah <sebrinm9@gmail.com>
2026-06-26 20:42:39 +0300

    Update api.ts

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,22 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://frontend-3rbz.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "...",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Attach JWT token automatically to every request
 API.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
+
 export default API;
