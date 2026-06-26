2c5a0b53ee2bba8beefedc2c955712d65ee09a79
sebrinamusbah <sebrinm9@gmail.com>
2026-06-27 01:08:20 +0300

    Update api.ts

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -9,11 +9,13 @@ const API = axios.create({
   },
 });
 
+// Attach token automatically
 API.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
 
     if (token) {
+      config.headers = config.headers || {};
       config.headers.Authorization = `Bearer ${token}`;
     }
   }
